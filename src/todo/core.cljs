8fa4aa7628fda842905ad1023ac7caffb928693b
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-02-02 17:14:50 +0100

    Update add-function

--- src/todo/core.cljs
+++ src/todo/core.cljs
@@ -6,8 +6,8 @@
 (def init-data
   {:count 0
    :todo/items [{:title "Liste irgendwie darstellen" :done? true}
-                {:title "Abgeschlossene Items durchstreichen" :done? false}
-                {:title "Abgeschlossene Items ausgrauen" :done? false}
+                {:title "Abgeschlossene Items durchstreichen" :done? true}
+                {:title "Abgeschlossene Items ausgrauen" :done? true}
                 {:title "Per Klick Items abhaken" :done? true}
                 {:title "Fertig werden, weil alle nach Hause wollen" :done? false}
                 {:title "cljs / om f*** ups" :done? false}]})
@@ -15,75 +15,68 @@
 ;; -----------------------------------------------------------------------------
 ;; Parsing
 
+(defn create-item [state title]
+  (-> state
+      (update-in [:todo/items] into {:todo/by-title title})
+      (assoc-in [:todo/by-title title] {:title title :done? false})))
+
 (defn get-items [state key]
   (let [st @state]
-    (into [] (map #(get-in st %)) (get st key))))
+    (mapv #(get-in st %) (get st key))))
 
 (defmulti read om/dispatch)
 (defmethod read :todo/items
-  [{:keys [state]} key params]
+  [{:keys [state]} key _]
   {:value (get-items state key)})
 
 (defmulti mutate om/dispatch)
 (defmethod mutate 'todo/toggle
   [{:keys [state]} _ {:keys [title done?]}]
-  {:action (fn [] (swap! state update-in [:todo/by-title title :done?] #(not done?)))})
+  {:action (swap! state update-in [:todo/by-title title :done?] #(not done?))})
 
 (defmethod mutate 'todo/add
-  [{:keys [state]} _ {:keys [value]}]
-  {:action (fn [] (do
-                   (swap! state update-in [:todo/items] into {:todo/by-title value})
-                   (swap! state update-in [:todo/by-title] assoc value {:title value :done? false})))})
-;; (om/transact! reconciler `[(todo/add {:value "foo"})])
+  [{:keys [state]} _ {:keys [title]}]
+  {:action (fn [] (swap! state create-item title))})
+
+;; (om/transact! reconciler '[(increment)])
+;; (om/transact! reconciler '[(todo/add {:title "foo2"})])
+
 
 ;; -----------------------------------------------------------------------------
 ;; Auxiliary
 
-(defn strike-through [done?]
+(defn line-through [done?]
   (if done?
     #js {:textDecoration "line-through"}
     #js {}))
+
+(defn add-item [this]
+  (dom/input #js {:className "form-control"
+                  :placeholder "Irgendwas wollte ich doch noch erledigen..."
+                  :onKeyDown #(when (= (.-key %) "Enter")
+                                (om/transact! this `[(todo/add {:title ~(.. % -target -value)})]))}))
 
 ;; -----------------------------------------------------------------------------
 ;; Components
 
 (defui Item
-  static om/Ident
-  (ident [this {:keys [title]}]
-         [:todo/by-title title])
   static om/IQuery
   (query [this]
          [:title :done?])
+  static om/Ident
+  (ident [this {:keys [title]}]
+         [:todo/by-title title])
   Object
   (render [this]
           (let [{:keys [title done?] :as props} (om/props this)]
-            (dom/div #js {:className (str "row" (when done? " text-muted"))}
-                     (dom/div #js {:className "col-xs-1"}
-                              (dom/input #js {:type "checkbox"
-                                              :checked done?
-                                              :onChange #(om/transact! this `[(todo/toggle ~props)])}))
-                     (dom/div #js {:className "col-xs-11"
-                                   :style (strike-through done?)}
-                              title)))))
+            (dom/div nil
+                     (dom/input #js {:type "checkbox"
+                                     :checked done?
+                                     :onChange #(om/transact! this `[(todo/toggle ~props)])})
+                     (dom/span #js {:className (when done? "text-muted")
+                                    :style (line-through done?)}
+                               title)))))
 (def item (om/factory Item))
-
-(defn todo-add [this]
-  (dom/input #js {:className "form-control"
-                  :placeholder "do something"
-                  :onKeyDown #(when (= (.-key %) "Enter")
-                                (om/transact! this `[(todo/add {:value ~(.. % -target -value)})]))}))
-
-(defui TodoList
-  Object
-  (render [this]
-          (let [items (om/props this)]
-            (dom/div #js {:className "panel panel-default"}
-                     (dom/div #js {:className "panel-heading"}
-                              "Todo List")
-                     (dom/div #js {:className "panel-body"}
-                              (map item items)
-                              (todo-add this))))))
-(def todo-list (om/factory TodoList))
 
 (defui Main
   static om/IQuery
@@ -92,16 +85,20 @@
   Object
   (render [this]
           (let [{:keys [todo/items]} (om/props this)]
-            (todo-list items))))
+            (dom/div #js {:className "panel panel-default"}
+                     (dom/div #js {:className "panel-heading"}
+                              "Todos")
+                     (dom/div #js {:className "panel-body"}
+                              (map item items)
+                              (add-item this))))))
 
-(defonce reconciler
+(def reconciler
   (om/reconciler
    {:state  init-data
     :parser (om/parser {:read read :mutate mutate})}))
 
 (om/add-root! reconciler
               Main (gdom/getElement "app"))
-
 
 ;; -----------------------------------------------------------------------------
 
@@ -110,6 +107,11 @@
 ;; norm-data
 
 ;; Time Travelling
-;; (om/from-history reconciler #uuid "be4b73d9-06d1-4473-8100-5340a487f507")
+;; (om/from-history reconciler #uuid "719e70ad-cca7-467d-96f1-eea41a5648fd")
 ;; Aktueller Zustand
-;; (om/app-state reconciler)
+;; @(om/app-state reconciler)
+
+
+;; (defmethod mutate 'increment
+;;   [{:keys [state]} _ _]
+;;   {:action (fn [] (swap! state update-in [:count] inc))})
